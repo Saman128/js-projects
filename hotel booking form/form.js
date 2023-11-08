989a56a078613ee98ad33dc49c638af3b938c63b
saman seher <127299155+Saman128@users.noreply.github.com>
2023-11-08 05:14:22 -0800

    Add files via upload

---
+++ hotel booking form/form.js
@@ -0,0 +1,29 @@
+function set(){
+    var x=document.querySelector(".room");
+    var y=document.querySelector(".noom");
+    var advance1=document.querySelector(".advance1");
+    var adv1=advance1.value;
+    var plus=1000*adv1;
+    var advance=document.querySelector(".advance");
+    var adv=advance.value;
+    var value1=x.value;
+    var value2=y.value;
+    var SUM=parseInt(value1)+parseInt(value2)+plus;
+    var sub=adv;
+    var rem=SUM-sub;
+    console.log(rem);
+    var disp=document.querySelector(".balance2");
+    var disp1=document.querySelector(".balance");
+    disp.innerHTML=SUM;
+    var name="rem...";
+    disp1.innerHTML=rem+name;
+   
+
+
+
+}
+const btn=document.querySelector(".btn");
+btn.addEventListener("click",button);
+function button(){
+    alert("Booking Successfuly......");
+}
